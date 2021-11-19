ab8233e6545ccb302b372c04bbf3b13bae920f87
dingyu15 <wdy22@hotmail.com>
2021-11-19 13:15:51 +0800

    update code to display properties for sale

--- property/src/components/property/Properties-SearchSaleRent.js
+++ property/src/components/property/Properties-SearchSaleRent.js
@@ -1,16 +1,49 @@
+import React, { useEffect, useState } from 'react';
 import API from '../../common_functions/API';
 import './property.css';
 
 export function PropertiesSearch({searchParameter}){
+    
     return(<>
         <h2>This is content for retrieving selected properties according to search parameter.</h2>
     </>)
 };
 
 export function PropertiesSale(){
-    return(<>
-        <h2>This is content for all properties listed for sale.</h2>
-    </>)
+    const [propertiesSale, setPropertiesSale] = useState('');
+    let propertiesSaleArray = [];
+
+    useEffect(() => {
+        API.get('/general/properties/search/isSale/true')
+        .then((response) => {
+            const propertiesSaleData = response.data.data;
+            setPropertiesSale(propertiesSaleData);
+        })
+        .catch(error => console.error(`Error: ${error}`));
+    }, []);
+
+    propertiesSaleArray = propertiesSale;
+
+    if (propertiesSaleArray.length > 0) {
+       console.log(propertiesSaleArray);
+
+        return ( 
+            <div className = "propertiesSale" >
+            <h2>List of properties for sale</h2>
+                {propertiesSaleArray.map(properties =>
+                <ul key={properties.id}>
+                   <li><b>{properties.location}</b></li>
+                   <li>Price: ${properties.price}</li>
+                   <li>{properties.noOfBedrooms} bedrooms</li>
+                </ul>
+                )}
+            </div>
+        );  
+    } else {
+        return(<>
+            <h2>There is no properties listed for sale.</h2>
+        </>)
+    }  
 };
 
 export function PropertiesRent(){
